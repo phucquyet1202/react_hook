1bf0da5e2021705a629d09b4546ea9fa539c01e2
phucquyet1202 <phucquyet1222003@gmail.com>
2023-03-11 22:17:14 +0700

    modified:   src/App.js 	modified:   src/components/nav_bar/nav.scss

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/nav_bar/nav';
+import { useState } from 'react';
 
 function App() {
-  const name = 'react'
+  const [name, setName] = useState('react by Quyet')
+  const handelEven = () => {
+    console.log(name);
+    const result = document.getElementById('input').value
+    setName(result)
+  }
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>hello wourld with {name}</h1>
+        <input id='input' type="text" name="" />
+        <button type="button" onClick={() => handelEven()}>click</button>
+
       </header>
     </div>
   );
