d0bce0d6cc412196dfb9ec15bdd21da0a84fd38f
phucquyet1202 <phucquyet1222003@gmail.com>
2023-03-11 17:04:24 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav_bar/nav';
 
 function App() {
+  const name = 'react'
   return (
-    // jsx
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello world with react</h1>
+        <h1>hello wourld with {name}</h1>
       </header>
     </div>
   );
---
+++ src/components/nav_bar/nav.js
@@ -0,0 +1,12 @@
+import './nav.scss'
+const Nav = () => {
+    return (
+        <div className="topnav">
+            <a className="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    )
+}
+export default Nav
